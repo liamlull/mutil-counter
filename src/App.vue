b29f83235eb4e8e9446e62efebae0dfdf52eda61
LULI3 <luanyuanshizhu@126.com>
2019-07-21 15:40:33 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <span>{{ times }}</span><input v-model="times">
+    <counter-group :title="parseInt(times==''?0:times)"/>
+  </div>
+</template>
+
+<script>
+import counterGroup from './components/count/counterGroup.vue'
+
+export default {
+  name: 'app',
+  components: {
+      counterGroup
+  },data(){
+      return {
+          times:0,
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/count/counterGroup.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <counter v-for=" n in title"/>
+    </div>
+</template>
+
+<script>
+    import counter from './counter'
+    export default {
+        name: "counterGroup",
+        components: {
+            counter
+        },
+        props:[
+            'title'
+        ],
+    }
+</script>
+
+<style scoped>
+
+</style>
